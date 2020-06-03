30d2120b1393a8cd6077b09bf5ffdf94e6914498
DominicHudson102983679 <61397178+DominicHudson102983679@users.noreply.github.com>
2020-06-03 12:02:13 +1000

    .ts input finally working

--- dist/bookmark.js
+++ dist/bookmark.js
@@ -6,11 +6,41 @@ var urlInput =  document.getElementById('new-url');
 var newList =  document.getElementById("bookmark-list");
 */
 var addBtn = document.querySelector('.add-div');
-if (addBtn == null) {
-    alert("addBtn not found");
+var titleInput = document.querySelector('.new-title');
+var urlInput = document.querySelector('.new-url');
+var bookmarkList = document.querySelector('.bookmark-list');
+if (addBtn == null || titleInput == null || urlInput == null || bookmarkList == null) {
+    alert("bookmark is null");
 }
 else {
-    addBtn.onclick = function () {
-        alert("hahahaha");
+    addBtn.onclick = function addBookmark() {
+        // creating new bookmark div
+        var bookmarkDiv = document.createElement('div');
+        bookmarkDiv.classList.add('vgbhfgdgdfg');
+        // creating li element
+        var newBookmark = document.createElement('li');
+        newBookmark.innerText = 'hey';
+        newBookmark.classList.add('todo-item');
+        bookmarkDiv.appendChild(newBookmark);
+        //completed button
+        var completedButton = document.createElement('button');
+        completedButton.innerText = '<is complted working>';
+        completedButton.classList.add("complete-btn");
+        bookmarkDiv.appendChild(completedButton);
+        //trash button
+        var trashButton = document.createElement('button');
+        trashButton.innerText = 'is trash working';
+        trashButton.classList.add("trash-btn");
+        bookmarkDiv.appendChild(trashButton);
+        //
+        bookmarkList.appendChild(bookmarkDiv);
+        /*
+        var titleNode = document.createTextNode(titleInput.value);
+        var urlNode = document.createTextNode(urlInput.value);
+        li.appendChild(titleNode);
+        li.appendChild(urlNode);
+        titleInput.innerHTML = "";
+        urlInput.innerHTML = "";
+        */
     };
 }
--- src/bookmark.ts
+++ src/bookmark.ts
@@ -7,11 +7,49 @@ var newList =  document.getElementById("bookmark-list");
 
 var addBtn =  document.querySelector('.add-div') as HTMLButtonElement | null;
 
+const titleInput = document.querySelector('.new-title') as HTMLInputElement | null;
+const urlInput = document.querySelector('.new-url') as HTMLInputElement | null;
 
-if(addBtn == null){
-    alert("addBtn not found");
+const bookmarkList = document.querySelector('.bookmark-list') as HTMLUListElement | null;
+
+
+
+if(addBtn == null || titleInput == null || urlInput == null || bookmarkList == null){
+    alert("bookmark is null");
 } else {
-addBtn.onclick = function () {
-    alert("hahahaha");
-}}
+addBtn.onclick = function addBookmark() {
 
+    // creating new bookmark div
+    const bookmarkDiv = document.createElement('div');
+    bookmarkDiv.classList.add('vgbhfgdgdfg');
+    // creating li element
+    var newBookmark = document.createElement('li');
+
+    newBookmark.innerText = 'hey';
+    newBookmark.classList.add('todo-item')
+    bookmarkDiv.appendChild(newBookmark);
+    //completed button
+    const completedButton = document.createElement('button');
+    completedButton.innerText = '<is complted working>';
+    completedButton.classList.add("complete-btn");
+    bookmarkDiv.appendChild(completedButton);
+    //trash button
+    const trashButton = document.createElement('button');
+    trashButton.innerText = 'is trash working'
+    trashButton.classList.add("trash-btn");
+    bookmarkDiv.appendChild(trashButton);
+    //
+    bookmarkList.appendChild(bookmarkDiv);
+
+
+    /*
+    var titleNode = document.createTextNode(titleInput.value);
+    var urlNode = document.createTextNode(urlInput.value);
+    li.appendChild(titleNode);
+    li.appendChild(urlNode);
+    titleInput.innerHTML = "";
+    urlInput.innerHTML = "";
+    */
+}
+
+}
